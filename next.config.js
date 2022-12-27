ec8a2ebe9d36fd769995ae3e8eafdd91ea3502c4
nate-d3v <nate.d3v@gmail.com>
2022-12-27 10:37:08 -0300

    Test wasm fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	webpack: function (config) {
-		config.experiments = { asyncWebAssembly: true };
+	webpack: function (config, { isServer, dev }) {
+		config.output.webassemblyModuleFilename =
+			isServer && !dev
+				? '../static/wasm/[modulehash].wasm'
+				: 'static/wasm/[modulehash].wasm';
+		config.experiments = { ...config.experiments, asyncWebAssembly: true };
 		return config;
 	},
 };
